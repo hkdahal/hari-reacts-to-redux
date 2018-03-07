7b78334c8c4abe705eb67fb23d90ce9a44f12eaf
Hari Dahal <hxd1478@rit.edu>
2018-03-07 01:01:37 -0500

    react-redux connected

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
+import { Provider } from "react-redux";
 import configureStore from "./stores/configureStore";
 import * as actions from "./actions";
 import Stream from "./components/Stream";
@@ -16,10 +17,12 @@ const store = configureStore();
 store.dispatch(actions.setTracks(tracks));
 
 const App = () => (
-  <div>
-    <h1>Hari getting into react/redux </h1>
-    <Stream />,
-  </div>
+  <Provider store={store}>
+    <div>
+      <h1>Hari getting into react/redux </h1>
+      <Stream />
+    </div>
+  </Provider>
 );
 
 render(<App />, document.getElementById("root"));
